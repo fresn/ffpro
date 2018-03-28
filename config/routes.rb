a922470d25bf0b936bc47f9d08156439aeea80cc
YimingMa <mz19843@gmail.com>
2018-03-28 14:11:24 -0500

    customer running

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :customers, singular: :customer
+  devise_for :customers,
+             singular: :customer,
+             path: "/", path_names:
+                 {
+                   sign_in:  "login",
+                   sign_out:  "logout"
+                 }
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
