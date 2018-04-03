934b17bd644adece328f47814c61a8dc80694a32
YimingMa <mz19843@gmail.com>
2018-04-03 14:47:43 -0500

    add Unit to ActiveAdmin add Validation to Unit

--- app/models/unit_backend.rb
+++ app/models/unit_backend.rb
@@ -1,3 +1,5 @@
 class UnitBackend < ApplicationRecord
   validates :name, uniqueness: true
+  validates :ToDefaultRate ,numericality: {:greater_than_or_equal_to => 0}
+  validates :kind,inclusion: {in:["Length","Volume"],message:"Kind must be one fo Length or Volume or Mass"}
 end
